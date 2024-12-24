d62efacdc1cf196192ae2f9da4481e2c51a83c2c
shrawan <shrawan.prajapat.999@gmail.com>
2024-12-24 17:47:06 +0530

    Pr-5 Done

---
+++ pr/Pr-5-Movie Project/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const port = 8010;
+
+const app = express();
+
+const path = require('path');
+
+const database = require('./config/db')
+database();
+
+app.set('view engine', 'ejs');
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use('/img', express.static('img'));
+
+app.use('/', require('./routes/indexRoute'));
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    console.log(`server is start on port :- ${port}`);
+
+})
